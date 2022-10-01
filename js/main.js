fe8ea9aaf79302052e4ff5d1d8202af88f4713f6
Daniel Anyanwu <73966734+AIDaniel1@users.noreply.github.com>
2022-10-01 05:30:37 -0700

    js commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
  const coundown = () => {
-    const countDate = new Date('October 20, 2022 00:00:00').getTime();
+    const countDate = new Date('October 25, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
